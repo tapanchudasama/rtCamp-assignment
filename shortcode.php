19f3c5fdb2a4e95351a563e7d8997cf510ad9198
inflame07 <tapan9740@gmail.com>
2019-09-10 19:39:45 +0530

    shortcode.php modified

--- shortcode.php
+++ shortcode.php
@@ -1,8 +1,14 @@
 <?php
-// Function to add text shortcode to posts and pages
-function text_shortcode()
+// [book] Shortcode
+function short_code($atts)
 {
-    return 'This is a simple text shortcode. Enjoy!';
+    $atts = shortcode_atts(array(
+        'id' => '1',
+        'author_name' => 'Tapan',
+        'category' => 'Drama',
+        'tag' => 'chetan',
+        'publisher' => 'harper collins'
+    ), $atts, 'book');
+    return " <b>PostID:</b> {$atts['id']}<br><b>Author Name</b>: {$atts['author_name']}<br><b>Category:</b> {$atts['category']}<br><b>Tags:</b> {$atts['tag']}<br><b>Publisher:</b> {$atts['publisher']}";
 }
-
-add_shortcode('text', 'text_shortcode');
+add_shortcode('book', 'short_code');
