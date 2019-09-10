dbf2f5e39b6739736fd1c51a0cbf2a053de02bcc
inflame07 <tapan9740@gmail.com>
2019-09-10 11:48:14 +0530

    Added custom non-hierarchial category Book Tags

--- wp-book.php
+++ wp-book.php
@@ -138,3 +138,39 @@ function register_taxonomy_book_category()
     ];
     register_taxonomy('book_category', array('book'), $args);
 }
+
+add_action('init', 'create_book_taxonomies', 0);
+
+function create_book_taxonomies()
+{
+    // Add new taxonomy, NOT hierarchical (like tags)
+    $labels = array(
+        'name'                       => _x('Book Tags', 'taxonomy general name'),
+        'singular_name'              => _x('Book Tag', 'taxonomy singular name'),
+        'search_items'               => __('Search Tags'),
+        'popular_items'              => __('Popular Tags'),
+        'all_items'                  => __('All Tags'),
+        'parent_item'                => null,
+        'parent_item_colon'          => null,
+        'edit_item'                  => __('Edit Tag'),
+        'update_item'                => __('Update Tag'),
+        'add_new_item'               => __('Add New Tag'),
+        'new_item_name'              => __('New Tag Name'),
+        'separate_items_with_commas' => __('Separate tags with commas'),
+        'add_or_remove_items'        => __('Add or remove tags'),
+        'choose_from_most_used'      => __('Choose from the most used tags'),
+        'not_found'                  => __('No tags found.'),
+        'menu_name'                  => __('Book Tags'),
+    );
+
+    $args = array(
+        'hierarchical'          => false,
+        'labels'                => $labels,
+        'show_ui'               => true,
+        'show_admin_column'     => true,
+        'update_count_callback' => '_update_post_term_count',
+        'query_var'             => true,
+        'rewrite'               => array('slug' => 'book_tag'),
+    );
+    register_taxonomy('book_tag', 'book', $args);
+}
